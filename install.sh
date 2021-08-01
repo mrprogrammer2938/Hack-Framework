413fd5e5e088d062a011219893203955146bff9c
Mr.nope <78996423+mrprogrammer2938@users.noreply.github.com>
2021-08-01 09:31:09 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 # This code write by Mr.nope
-# Hack-Framework v2.2.0
-prinf '\033]2;Installing\a'
-clear
-echo "Installing..."
-sleep 2
-sudo apt install python
-sudo apt install python3
-sudo apt install pip
-pip install requirements.txt
-sleep 2
-clear
-echo "Finish! Installing..."
-sleep 2
-chmod +x hack
-cd Update && chmod +x hack
-echo ""
+# Hack-Framework v2.2.2
+if [[ "$(id -u)" -ne 0 ]];
+then
+  echo "
+Please, Run This Program as Root!
+"
+  exit 1
+fi
+function main() {
+     prinf '\033]2;Installing\a'
+     clear
+     echo "Installing..."
+     sleep 2
+     apt install python
+     apt install python3
+     apt install pip
+     pip install requirements.txt
+     sleep 2
+     clear
+     echo "Finish! Installing..."
+     sleep 2
+     chmod +x hack
+     cd Update && chmod +x hack
 echo "
 usage:
      ./hack"
-echo ""
-exit 1
+     echo ""
+     exit 1
+     }
+main
